4b16d476ef717073a0d6a1b76cd6398a09282acb
Aneta <aneta.strzyzewska@outlook.com>
2023-07-19 17:45:33 +0200

    Generate books component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
