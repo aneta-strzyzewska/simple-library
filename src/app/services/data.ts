b21c1c4ce84c7f8d5e01b03b9adab8f146e427ef
Aneta <aneta.strzyzewska@outlook.com>
2023-07-23 08:36:42 +0200

    Fill out the remaining data and move the 'database' to a separete file

---
+++ src/app/services/data.ts
@@ -0,0 +1,79 @@
+import { Book } from "../types";
+
+export const books: Book[] = [
+    { 
+      title: 'Guards! Guards!', 
+      author: 'Terry Pratchett', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1431127356i/64216.jpg', 
+      summary: `This is where the dragons went. They lie... not dead, not asleep, but... dormant. And although the space they occupy isn't like normal space, nevertheless they are packed in tightly. They could put you in mind of a can of sardines, if you thought sardines were huge and scaly. And presumably, somewhere, there's a key...`, 
+      isbn: '9780061020643' 
+    },
+    { 
+      title: 'Uprooted', 
+      author: 'Naomi Novik', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1498295085i/35507099.jpg', 
+      summary: `“Our Dragon doesn't eat the girls he takes, no matter what stories they tell outside our valley. We hear them sometimes, from travelers passing through. They talk as though we were doing human sacrifice, and he were a real dragon. Of course that's not true: he may be a wizard and immortal, but he's still a man, and our fathers would band together and kill him if he wanted to eat one of us every ten years. He protects us against the Wood, and we're grateful, but not that grateful.”`, 
+      isbn: '9780804179034' },
+    { 
+      title: 'Ancillary Justice', 
+      author: 'Ann Leckie', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1597476110i/17333324.jpg', 
+      summary: `On a remote, icy planet, the soldier known as Breq is drawing closer to completing her quest. Once, she was the Justice of Toren - a colossal starship with an artificial intelligence linking thousands of soldiers in the service of the Radch, the empire that conquered the galaxy. Now, an act of treachery has ripped it all away, leaving her with one fragile human body, unanswered questions, and a burning desire for vengeance.`, 
+      isbn: '9780316246620' },
+    { 
+      title: 'Good Omens', 
+      author: 'Terry Pratchett, Neil Gaiman', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1615552073i/12067.jpg', 
+      summary: `According to The Nice and Accurate Prophecies of Agnes Nutter, Witch (the world's only completely accurate book of prophecies, written in 1655, before she exploded), the world will end on a Saturday. Next Saturday, in fact. Just before dinner.
+
+      So the armies of Good and Evil are amassing, Atlantis is rising, frogs are falling, tempers are flaring. Everything appears to be going according to Divine Plan. Except a somewhat fussy angel and a fast-living demon—both of whom have lived amongst Earth's mortals since The Beginning and have grown rather fond of the lifestyle—are not actually looking forward to the coming Rapture.
+      
+      And someone seems to have misplaced the Antichrist . . . `, 
+      isbn: '9780060853983' },
+    { 
+      title: 'Castles of Steel', 
+      author: 'Robert K. Massie', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1172707687i/209881.jpg', 
+      summary: `In August 1914 the two greatest navies in the world confronted each other across the North Sea. At first there were skirmishes, then battles off the coasts of England and Germany and in the far corners of the world, including the Falklands. The British attempted to force the Dardanelles with battleships - which led to the Gallipoli catastrophe. As the stalemate on the ground on the Western Front continued, the German Navy released a last strike against the British 'ring of steel'. The result was Jutland, a titanic and brutal battle between dreadnoughts. The knowledge, understanding and literary power Robert K. Massie brings to this story is unparalleled. There will never again be a war like this in which seagoing monsters hurl shells at each other until one side is destroyed. The story is driven by some of the most dramatically intriguing personalities in history: Churchill and Jacky Fisher, Jellicoe and Beatty. And then there were the powerful Germans - von Pohl, Scheer, Hipper, and the grand old fork-bearded genius Tirpitz. Castles of Steel is a book about leadership and command, bravery and timidity, genius and folly, qualities which are of course displayed magnificently by Robert K. Massie's literary mastery.`, 
+      isbn: '9781844134113' },
+    { 
+      title: 'Winter Tide', 
+      author: ' Ruthanna Emrys ', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1463670456i/29939089.jpg', 
+      summary: `After attacking Devil's Reef in 1928, the U.S. government rounded up the people of Innsmouth and took them to the desert, far from their ocean, their Deep One ancestors, and their sleeping god Cthulhu. Only Aphra and Caleb Marsh survived the camps, and they emerged without a past or a future.
+
+      The government that stole Aphra's life now needs her help. FBI agent Ron Spector believes that Communist spies have stolen dangerous magical secrets from Miskatonic University, secrets that could turn the Cold War hot in an instant, and hasten the end of the human race.
+      
+      Aphra must return to the ruins of her home, gather scraps of her stolen history, and assemble a new family to face the darkness of human nature.`, 
+      isbn: '9780765390905 ' 
+    },
+    { 
+      title: 'The Guns of August', 
+      author: 'Barbara W. Tuchman', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1437929026i/21393414.jpg', 
+      summary: `In this landmark, Pulitzer Prize-winning account, renowned historian Barbara W. Tuchman re-creates the first month of World War I: thirty days in the summer of 1914 that determined the course of the conflict, the century, and ultimately our present world. Beginning with the funeral of Edward VII, Tuchman traces each step that led to the inevitable clash. And inevitable it was, with all sides plotting their war for a generation. Dizzyingly comprehensive and spectacularly portrayed with her famous talent for evoking the characters of the war's key players, Tuchman's magnum opus is a classic for the ages.`, 
+      isbn: '9780026203104' },
+    { 
+      title: 'The Hobbit', 
+      author: 'J.R.R. Tolkien', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1546071216i/5907.jpg', 
+      summary: `In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort.`, 
+      isbn: '9780544045521 ' 
+    },
+    { 
+      title: 'Ancillary Sword', 
+      author: 'Ann Leckie', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1597476183i/20706284.jpg', 
+      summary: `Breq is a soldier who used to be a warship. Once a weapon of conquest controlling thousands of minds, now she has only a single body and serves the emperor.
+
+      With a new ship and a troublesome crew, Breq is ordered to go to the only place in the galaxy she would agree to go: to Athoek Station to protect the family of a lieutenant she once knew - a lieutenant she murdered in cold blood.`, 
+      isbn: '9780316246651' 
+    },
+    { 
+      title: 'Solaris', 
+      author: 'Stanisław Lem', 
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1498631519i/95558.jpg', 
+      summary: `When Kris Kelvin arrives at the planet Solaris to study the ocean that covers its surface, he finds a painful, hitherto unconscious memory embodied in the living physical likeness of a long-dead lover. Others examining the planet, Kelvin learns, are plagued with their own repressed and newly corporeal memories. The Solaris ocean may be a massive brain that creates these incarnate memories, though its purpose in doing so is unknown, forcing the scientists to shift the focus of their quest and wonder if they can truly understand the universe without first understanding what lies within their hearts.`, 
+      isbn: '9788838929106' 
+    },
+  ]
--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -1,41 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Book } from '../types';
+import { books } from './data'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryService {
 
-  private books: Book[] = [
-    { 
-      title: 'Guards! Guards!', 
-      author: 'Terry Pratchett', 
-      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1431127356i/64216.jpg', 
-      summary: `This is where the dragons went. They lie... not dead, not asleep, but... dormant. And although the space they occupy isn't like normal space, nevertheless they are packed in tightly. They could put you in mind of a can of sardines, if you thought sardines were huge and scaly. And presumably, somewhere, there's a key...`, 
-      isbn: '9780061020643' 
-    },
-    { 
-      title: 'Uprooted', 
-      author: 'Naomi Novik', 
-      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1550135418i/22544764.jpg', 
-      summary: `“Our Dragon doesn't eat the girls he takes, no matter what stories they tell outside our valley. We hear them sometimes, from travelers passing through. They talk as though we were doing human sacrifice, and he were a real dragon. Of course that's not true: he may be a wizard and immortal, but he's still a man, and our fathers would band together and kill him if he wanted to eat one of us every ten years. He protects us against the Wood, and we're grateful, but not that grateful.”`, 
-      isbn: '9780804179034' },
-    { 
-      title: 'Ancillary Justice', 
-      author: 'Ann Leckie', 
-      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1597476110i/17333324.jpg', 
-      summary: `On a remote, icy planet, the soldier known as Breq is drawing closer to completing her quest. Once, she was the Justice of Toren - a colossal starship with an artificial intelligence linking thousands of soldiers in the service of the Radch, the empire that conquered the galaxy. Now, an act of treachery has ripped it all away, leaving her with one fragile human body, unanswered questions, and a burning desire for vengeance.`, 
-      isbn: '9780316246620' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-    { title: '', author: '', cover: '', summary: ``, isbn: '' },
-  ]
+  private books: Book[]
 
-  constructor() { }
+  constructor() {
+    this.books = books;
+  }
 
   getBooks(): Book[] {
     return this.books;
